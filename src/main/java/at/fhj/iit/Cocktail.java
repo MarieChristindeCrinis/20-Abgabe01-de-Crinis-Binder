a6c3beec7a2840102fd977f29d9b3140abb17e50
MarieChristindeCrinis <62705069+MarieChristindeCrinis@users.noreply.github.com>
2020-05-11 17:27:11 +0200

    added some methods to the cocktail class in order to give the functionality of adding ingredients

--- src/main/java/at/fhj/iit/Cocktail.java
+++ src/main/java/at/fhj/iit/Cocktail.java
@@ -1,6 +1,12 @@
 package at.fhj.iit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cocktail extends Drink{
+
+    private List<Liquid> ingredients;
+
     /**
      * Creates a Drink object with given name
      *
@@ -8,6 +14,7 @@ public class Cocktail extends Drink{
      */
     public Cocktail(String name) {
         super(name);
+        this.ingredients = new ArrayList<>();
     }
 
     @Override
@@ -23,5 +30,37 @@ public class Cocktail extends Drink{
     @Override
     public boolean isAlcoholic() {
         return false;
+    }
+
+    /**
+     * Adds a liquid to the ingredients list
+     * @param l the liquid that should be added
+     */
+    public void addIngredient(Liquid l) {
+        this.ingredients.add(l);
+    }
+
+    /**
+     * Removes a liquid from the list of ingredients
+     * @param l the liquid to be removed
+     */
+    public void removeIngredient(Liquid l) {
+        this.ingredients.remove(l);
+    }
+
+    /**
+     * Removes a liquid from the list of ingredients at a given index
+     * @param index the position of the ingredient to be removed
+     */
+    public void removeIngredient(int index) {
+        this.ingredients.remove(index);
+    }
+
+    /**
+     * Returns the recipe for this cocktail 
+     * @return the recipe for the cocktail
+     */
+    public String recipe() {
+
     }
 }
