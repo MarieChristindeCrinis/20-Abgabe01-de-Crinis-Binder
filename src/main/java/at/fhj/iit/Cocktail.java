d11b9c9838fdf850229504740de5fac952ebe25a
LauraSophieBinder <62705055+LauraSophieBinder@users.noreply.github.com>
2020-05-11 17:14:38 +0200

    created the subclass Cocktail

---
+++ src/main/java/at/fhj/iit/Cocktail.java
@@ -0,0 +1,27 @@
+package at.fhj.iit;
+
+public class Cocktail extends Drink{
+    /**
+     * Creates a Drink object with given name
+     *
+     * @param name name of the drink
+     */
+    public Cocktail(String name) {
+        super(name);
+    }
+
+    @Override
+    public double getVolume() {
+        return 0;
+    }
+
+    @Override
+    public double getAlcoholPercent() {
+        return 0;
+    }
+
+    @Override
+    public boolean isAlcoholic() {
+        return false;
+    }
+}
